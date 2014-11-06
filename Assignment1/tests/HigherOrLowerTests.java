e977718f9ea19421d22c33b01c9d87daefbff3a1
shjelm <shjelm93@gmail.com>
2014-11-06 14:53:45 +0100

    Bytte projekt

---
+++ Assignment1/tests/HigherOrLowerTests.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class HigherOrLowerTests {
+	@Test
+	public void testConstructor(){
+	}
+
+}
--- Assignment1/tests/SticksGameTests.java
+++
@@ -1,9 +0,0 @@
-import org.junit.*;
-
-public class SticksGameTests {
-	@Test
-	public void testConstructor(){
-		new SticksGame();
-	}
-
-}
