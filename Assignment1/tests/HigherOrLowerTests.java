ff00509d699a97da23d730d7cd660bc7dfe1bd0b
shjelm <shjelm93@gmail.com>
2014-11-06 15:36:25 +0100

    RED

--- Assignment1/tests/HigherOrLowerTests.java
+++ Assignment1/tests/HigherOrLowerTests.java
@@ -5,8 +5,13 @@ import org.junit.*;
 public class HigherOrLowerTests {
 	
 	@Test(expected = IllegalArgumentException.class)
-	public void testConstructor(){
+	public void testConstructorNull(){
 		new HighLow(null);
+	}
+	
+	@Test
+	public void testConstructor(){		
+		new HighLow(9);
 	}
 
 }
