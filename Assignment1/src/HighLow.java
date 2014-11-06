fd99a77da14e51bed469367a104e822e763fe759
shjelm <shjelm93@gmail.com>
2014-11-06 15:43:36 +0100

    GREEN

--- Assignment1/src/HighLow.java
+++ Assignment1/src/HighLow.java
@@ -2,8 +2,10 @@
 public class HighLow {
 
 	public HighLow(Object object) {
-		if(object == null || object.equals(Integer.valueOf(-9)))
+		if(object == null || object.equals(Integer.valueOf(-9)) 
+				|| object.equals(Integer.valueOf(101))){
 			throw new IllegalArgumentException();
+		}
 	}
 
 }
