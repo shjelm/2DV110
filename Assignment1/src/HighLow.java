bd1327012eaf25fe10f78eaca0c6a8ed7467f056
shjelm <shjelm93@gmail.com>
2014-11-07 17:15:41 +0100

    REFACTOR

--- Assignment1/src/HighLow.java
+++ Assignment1/src/HighLow.java
@@ -10,16 +10,16 @@ public class HighLow {
 		secretNumber = generateRandomNumber();
 	}
 
-	public static int generateRandomNumber() {
-		int min = 1, max = 100;
-		Random rand = new Random();
-		return rand.nextInt((max-min)+1)+min;
-	}
-
 	public boolean checkIfEqualToSecretNumber(int a) {
 		if(a == secretNumber)
 			return true;
 		return false;
+	}
+
+	public static int generateRandomNumber() {
+		int min = 1, max = 100;
+		Random rand = new Random();
+		return rand.nextInt((max-min)+1)+min;
 	}
 
 }
