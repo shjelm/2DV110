f287b9b0add1bdd3b4137837cbcee38ead2420da
shjelm <shjelm93@gmail.com>
2014-11-20 14:53:47 +0100

    GREEN

--- Assignment1/src/HighLowView.java
+++ Assignment1/src/HighLowView.java
@@ -1,5 +1,7 @@
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.Scanner;
 
 
 public class HighLowView {
@@ -7,10 +9,19 @@ public class HighLowView {
 	PrintStream out;
 
 	public HighLowView(InputStream in, PrintStream out){
-		if(in == null || out == null){
+		if(in == null && out == null){
 			throw new IllegalArgumentException();
 		}
 		this.in = in;
 		this.out = out;
+	}
+
+	public int readInt() throws IOException {
+//		Scanner scan = new Scanner(in);
+//		System.out.println(scan.nextInt());
+//		String guess = scan.next();
+//		return guess;
+		
+		return this.in.read();
 	}
 }
